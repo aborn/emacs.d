84732e0d9a452580c05f5b68ebfdc6794a4a0c76
aborn <loveaborn@foxmail.com>
2014-05-06 10:29:41 +0800

    add .gitignore

--- utils/buffer-dealing.el
+++ utils/buffer-dealing.el
@@ -2,14 +2,8 @@
 
 (defun buffer-mode (buffer-or-string)
   "Returns the major mode associated with a buffer."
-
   (with-current-buffer buffer-or-string
     major-mode))
-
-;; (defun get-buffer-mode-name (buffer-or-string)
-;;   "Get the buffer major mode name."
-;;   (interactive "bBuffer Name:")
-;;   (message (buffer-mode buffer-or-string)))
 
 (defun get-buffer-mode-name (buffer-or-string)
   "Get the buffer major mode name."
