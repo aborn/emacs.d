903c1baa47820439577d181fcb27f10912c240cc
aborn <loveaborn@foxmail.com>
2014-05-07 21:04:56 +0800

    do not push before when using move-swift.el

--- utils/move-swift.el
+++ utils/move-swift.el
@@ -2,7 +2,6 @@
 (defun move-middle-of-line (arg)
   "Move point to the middle of line current displayed" 
   (interactive "p")
-  (push-mark)
   (message "move to middle of line, cur=%d." (current-column))
   (end-of-line)
   (backward-char (/ (current-column) 2)))
@@ -10,12 +9,10 @@
 (defun move-forward-by-five (arg)
   "Move point forward by five lines"
   (interactive "p")
-  (push-mark)
   (forward-line 5))
 
 (defun move-backward-by-five (arg)
 "Move point backward by five lines"
   (interactive "p")
-  (push-mark)
   (forward-line -5))
 
