f0756f3c53d55e0ff8c5485afe4701b385ac5157
aborn <loveaborn@foxmail.com>
2014-05-10 08:13:49 +0800

    add ab/rename function for rename file and buffer

--- utils/buffer-dealing.el
+++ utils/buffer-dealing.el
@@ -74,4 +74,23 @@
     (make-shell (ab/get-default-shell-buffer-name)))
   (switch-to-buffer (ab/get-default-shell-buffer-name)))
 
+;; -----------------------------------------------------------------
+;; source: http://steve.yegge.googlepages.com/my-dot-emacs-file
+;; -----------------------------------------------------------------
+(defun ab/rename (new-name)
+  "Renames both current buffer and file it's visiting to NEW-NAME."
+  (interactive "sNew name: ")
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not filename)
+        (message "Buffer '%s' is not visiting a file!" name)
+      (if (get-buffer new-name)
+          (message "A buffer named '%s' already exists!" new-name)
+        (progn
+          (rename-file name new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil))))))
+
 (defalias 'ab/buffer-exists 'buffer-exists)
+(defalias 'ab/shell 'make-shell)
