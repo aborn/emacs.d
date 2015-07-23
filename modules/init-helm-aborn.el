3a5260ffdc4152ab9c73c3b91efab31e3ae39640
guobao.jiang <loveaborn@foxmail.com>
2015-07-23 10:26:05 +0800

    add helm-ls-git

--- modules/init-helm-aborn.el
+++ modules/init-helm-aborn.el
@@ -7,6 +7,7 @@
 ;; --------------------------------------------------------------------
 ;;(tv-require 'helm-config)
 (require 'helm-config)
+(require 'helm-ls-git)
 (add-to-list 'load-path "~/.emacs.d/site-lisp/helm")
 ;;; Enable Modes (This is loading nearly everything).
 ;;
