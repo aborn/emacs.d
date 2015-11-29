030de540df8ae055120a72220770cf458bcfeec3
蒋国宝 <loveaborn@foxmail.com>
2015-11-29 21:44:48 +0800

    add debug-funciton

---
+++ debug-function.el
@@ -0,0 +1,13 @@
+;; -----------------------------------------------------------------------------
+;; 打开某个函数的调用开关
+;; 如果是出错的时候debug,可以执行 M-x toggle-debug-on-error
+;; M-x toggle-debug-on-quit
+;; 快捷键 d 单步执行
+;;       c  继续
+;;       e  求取值
+;; -----------------------------------------------------------------------------
+;;(debug-on-entry 'helm-adaptive-maybe-load-history)
+;;(debug-on-entry 'helm-adaptive-save-history)
+
+;;(debug-on-entry 'helm-adapt-use-adaptive-p)
+;;(debug-on-entry 'helm-adaptive-store-selection)
