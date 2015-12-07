53da80629f3e606f28fd906cbb02a62ee922e24e
guobao.jiang <loveaborn@foxmail.com>
2015-12-07 09:29:55 +0800

    update

--- parts/web-part.el
+++ parts/web-part.el
@@ -7,5 +7,10 @@
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-
+(setq web-mode-enable-current-element-highlight t)   ;; 高亮当前行
+(setq web-mode-enable-current-column-highlight t)    ;; 高亮当前列
+;; auto-complete 源setting
+;; (setq web-mode-ac-sources-alist
+;;  '(("css" . (ac-source-css-property))
+;;    ("html" . (ac-source-words-in-buffer ac-source-abbrev))))
 (provide 'web-part)
