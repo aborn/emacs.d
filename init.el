e1c27c8db25446ec23ffd4e4a7af71ba96cabc15
guobao.jiang <loveaborn@foxmail.com>
2015-12-23 09:20:54 +0800

    setting ligatures for firacode font

--- init.el
+++ init.el
@@ -658,13 +658,36 @@
 ;; setting font for mac system
 ;; Fira Code font: https://github.com/tonsky/FiraCode
 ;; -----------------------------------------------------------------------------
-;; Setting English Font 
+;; Setting English Font
 (set-face-attribute
  'default nil :font "Fira Code 15")  ;;previous "Monaco 14"
 (when (window-system)
-  (set-frame-font "Fira Code 15")
-  (message "Fira Code turn on!"))
-
+  (set-default-font "Fira Code 15")
+  (message "Fira Code Font turn on.."))
+(let ((alist '((33 . ".\\(?:\\(?:==\\)\\|[!=]\\)")
+               (35 . ".\\(?:[(?[_{]\\)")
+               (38 . ".\\(?:\\(?:&&\\)\\|&\\)")
+               (42 . ".\\(?:\\(?:\\*\\*\\)\\|[*/]\\)")
+               (43 . ".\\(?:\\(?:\\+\\+\\)\\|\\+\\)")
+               (45 . ".\\(?:\\(?:-[>-]\\|<<\\|>>\\)\\|[<>}~-]\\)")
+               (46 . ".\\(?:\\(?:\\.[.<]\\)\\|[.=]\\)")
+               (47 . ".\\(?:\\(?:\\*\\*\\|//\\|==\\)\\|[*/=>]\\)")
+               (58 . ".\\(?:[:=]\\)")
+               (59 . ".\\(?:;\\)")
+               (60 . ".\\(?:\\(?:!--\\)\\|\\(?:\\$>\\|\\*>\\|\\+>\\|--\\|<[<=-]\\|=[<=>]\\||>\\)\\|[/<=>|-]\\)")
+               (61 . ".\\(?:\\(?:/=\\|:=\\|<<\\|=[=>]\\|>>\\)\\|[<=>~]\\)")
+               (62 . ".\\(?:\\(?:=>\\|>[=>-]\\)\\|[=>-]\\)")
+               (63 . ".\\(?:[:=?]\\)")
+               (92 . ".\\(?:\\(?:\\\\\\\\\\)\\|\\\\\\)")
+               (94 . ".\\(?:=\\)")
+               (123 . ".\\(?:-\\)")
+               (124 . ".\\(?:\\(?:|[=|]\\)\\|[=>|]\\)")
+               (126 . ".\\(?:[=@~-]\\)")
+			   )
+			 ))
+  (dolist (char-regexp alist)
+	(set-char-table-range composition-function-table (car char-regexp)
+						  `([,(cdr char-regexp) 0 font-shape-gstring]))))
 ;; Chinese Font 配制中文字体
 (dolist (charset '(kana han symbol cjk-misc bopomofo))
   (set-fontset-font (frame-parameter nil 'font)
