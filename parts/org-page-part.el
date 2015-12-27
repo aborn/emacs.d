09d73ee8ee79ff82ac459e781eb600e60cfef448
蒋国宝 <loveaborn@foxmail.com>
2015-12-27 23:39:25 +0800

    add exp.el

--- parts/org-page-part.el
+++ parts/org-page-part.el
@@ -6,6 +6,9 @@
 (setq op/site-sub-title "思考创造个性，个性成就自我！")
 (setq op/personal-github-link "https://github.com/aborn")
 (setq op/personal-avatar nil)
+
+(setq op/repository-org-branch "source") ;; default is "source"
+(setq op/repository-html-branch "gh-pages")  ;; default is "master"
 ;; (setq op/personal-avatar "http://m1.s1.dpfile.com/sc/api_res/weather/found_cloud.jpg")
 (setq op/personal-duoshuo-shortname "aborn")   ;; your duoshuo commenting system
 
