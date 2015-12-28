a73659f3227c75d037fd909d0314a40694f1948a
蒋国宝 <loveaborn@foxmail.com>
2015-12-28 22:18:02 +0800

    org-mode setting

--- parts/org-mode-part.el
+++ parts/org-mode-part.el
@@ -4,9 +4,9 @@
 ;; (setq org-todo-keywords
 ;; '((sequence "TODO" "FEEDBACK" "VERIFY" "|" "DONE" "DELEGATED")))
 (setq org-todo-keywords
-	  '((sequence "TODO" "ONGOING" "DONE" "DELEGATED")))
+	  '((sequence "TODO" "ONGOING" "DONE" "|" "CLOSED")))
 (setq org-log-done 'time)   ;;  setting close time
 (setq org-log-done 'note)   ;;  setting a log note
 ;; (set org-modules 'habits)
-
+(setq org-startup-folded nil)  ;; 打开org文件默认不展开所有
 (provide 'org-mode-part)
