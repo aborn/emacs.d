58403d9b554385ed48ac31cb0493fba001dcab78
guobao.jiang <loveaborn@foxmail.com>
2015-12-28 13:19:09 +0800

    add C-c p to publish blog

--- parts/org-page-part.el
+++ parts/org-page-part.el
@@ -12,5 +12,9 @@
 ;; (setq op/personal-avatar "http://m1.s1.dpfile.com/sc/api_res/weather/found_cloud.jpg")
 (setq op/personal-duoshuo-shortname "aborn")   ;; your duoshuo commenting system
 
-(op/do-publication t nil nil t)
+(global-set-key (kbd "C-c p") (lambda ()
+                                (interactive)
+                                (op/do-publication t nil nil t t)  ;; 最后一个参数表示自动push
+                                (message "发布完成!")))
+
 (provide 'org-page-part)
